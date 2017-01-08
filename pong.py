4bb0c3efa17a9bd5f5bc67cf236c7b733d20d14c
Matthew Goff <mtthwgff@gmail.com>
2017-01-08 13:18:30 -0500

    finished physics, added 4 color modes, added drag. Still bug occasionally

--- pong.py
+++ pong.py
@@ -1,101 +1,114 @@
-# A simple pong game
-# Author: Matthew Anderson, Kristina Striegnitz, John Rieffel
-# Winter 2017
-
 import pygame
 import random
+from math import pi
 from vectors import Vec_2D
+import sys
 
-width = 640
-height = 480
-num_balls = 100
+seed = random.randint(0, sys.maxint)
+print "seed = "+str(seed)
+myRand = random.Random(seed)
 
-speed_limit = 0.5
-size_limit = 10
+# Window Parameters
+width = 640*2
+height = 480*2
+
+# Ball parameters
+num_balls = 50
+initial_speed = 1           # pixels per "tick"
+radius_range = (10, 80)    # in pixels
+
+# Physics parameters
+drag = False
+drag_delay = 1000           # ticks to wait until drag starts
+drag_coefficient = -0.0001  # Try making it positive ;)
+inverse_mass = False
+
+# Display parameters
+color_scheme = "random"     # bounce, random, gradient, speed
+draw_velocity = False
 
 class Ball:
-
     def __init__(self, position=None, velocity=None, rgb=None, radius=None):
         if radius is None:
-            self.radius = size_limit
+            self.radius = myRand.randint(radius_range[0], radius_range[1])
         else:
             self.radius = radius
 
         if position is None:
-            self.position = Vec_2D(random.uniform(0+self.radius, width-self.radius),
-                                   random.uniform(0+self.radius, height-self.radius))
+            self.position = Vec_2D(myRand.uniform(0 + self.radius, width - self.radius),
+                                   myRand.uniform(0 + self.radius, height - self.radius))
         else:
             self.position = position
 
         if velocity is None:
-            self.velocity = Vec_2D(random.uniform(-speed_limit, speed_limit),
-                                   random.uniform(-speed_limit, speed_limit))
+            self.velocity = Vec_2D(myRand.uniform(-initial_speed, initial_speed),
+                                   myRand.uniform(-initial_speed, initial_speed))
         else:
             self.velocity = velocity
 
         if rgb is None:
-            self.color = pygame.color.Color(random.randint(0, 255),
-                                            random.randint(0, 255),
-                                            random.randint(0, 255))
+            self.color = pygame.color.Color(myRand.randint(0, 255),
+                                            myRand.randint(0, 255),
+                                            myRand.randint(0, 255),
+                                            1)
         else:
-            self.color = pygame.color.Color(rgb[0],rgb[1],rgb[2])
+            self.color = pygame.color.Color(rgb[0], rgb[1], rgb[2], 1)
 
-        self.next_position = self.position
         self.next_velocity = self.velocity
         self.next_color = self.color
 
-    def update(self, balls):
-
-        self.next_velocity = self.velocity
-
-        collision = False
-        if self.get_x() <= 0+self.radius:
-            collision_vec = Vec_2D(-1,0)
-            if collision_vec.dot(self.velocity) > 0:
-                self.next_velocity = self.next_velocity.add(Vec_2D(-2*self.velocity.get_x(), 0))
-                collision = True
-        elif self.get_x() >= width-self.radius:
-            collision_vec = Vec_2D(1, 0)
-            if collision_vec.dot(self.velocity) > 0:
-                self.next_velocity = self.next_velocity.add(Vec_2D(-2 * self.velocity.get_x(), 0))
-                collision = True
-        if self.get_y() <= 0+self.radius:
-            collision_vec = Vec_2D(0, -1)
-            if collision_vec.dot(self.velocity) > 0:
-                self.next_velocity = self.next_velocity.add(Vec_2D(0, -2*self.velocity.get_y()))
-                collision = True
-        elif self.get_y() >= height-self.radius:
-            collision_vec = Vec_2D(0, 1)
-            if collision_vec.dot(self.velocity) > 0:
-                self.next_velocity = self.next_velocity.add(Vec_2D(0, -2 * self.velocity.get_y()))
-                collision = True
-
-        self.next_position = self.position.add(self.next_velocity)
-
-        if collision:
-            self.next_color = pygame.color.Color(random.randint(0,255),random.randint(0,255),random.randint(0,255))
+    def collide(self, next_velocity):
+        if color_scheme == "bounce":
+            self.next_color = pygame.color.Color(myRand.randint(0, 255), myRand.randint(0, 255), myRand.randint(0, 255), 1)
         else:
             self.next_color = self.color
+        self.next_velocity = next_velocity
 
     def simulate(self):
+        acceleration = self.next_velocity.unit().scale(drag_coefficient*self.next_velocity.mag()**2)
+        if drag:
+            self.next_velocity = self.next_velocity.add(acceleration)
         self.velocity = self.next_velocity
-        self.position = self.next_position
+        self.position = self.position.add(self.velocity)
         self.color = self.next_color
 
     def get_radius(self):
         return self.radius
 
-    def get_pos(self):
-        return (self.get_x(),self.get_y())
+    def get_position(self):
+        return self.position
 
-    def get_x(self):
-        return self.position.get_x()
+    def get_velocity(self):
+        return self.velocity
 
-    def get_y(self):
-        return self.position.get_y()
+    def get_next_velocity(self):
+        return self.next_velocity
 
     def get_color(self):
-        return self.color
+        if color_scheme == "gradient" or color_scheme == "speed":
+            if color_scheme == "speed":
+                r = int((self.get_next_velocity().mag()*50)%255)
+                g = r
+                b = g
+            else:
+                r = int(255 * self.get_position().get_x() / width)
+                g = int(255 * self.get_position().get_y() / height)
+                b = int(255*self.get_radius()/radius_range[1])
+
+            r = min(max(0, r), 255)     # encase it shoots over
+            g = min(max(0, g), 255)
+            b = min(max(0, b), 255)
+
+            return pygame.color.Color(r, g, b, 1)
+        else:
+            return self.color
+
+    def get_mass(self):
+        if inverse_mass:
+            return 1/(pi*self.get_radius()**2)
+        else:
+            return pi*self.get_radius()**2
+
 
 def run_game():
 
@@ -106,35 +119,45 @@ def run_game():
 
     # Initialize balls
     my_balls = []
+
     for i in range(0, num_balls):
         my_balls += [Ball()]
 
-
     # The game loop starts here.
     keep_going = True
-    while (keep_going):
+    while keep_going:
+        if drag_delay == 0:
+            global drag
+            drag = True
+        elif drag_delay > 0:
+            global drag_delay
+            drag_delay -= 1
 
         # 1. Handle events.
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 keep_going = False
 
-
         # 2. Apply rules of game world
-        for ball in my_balls:
-            ball.update(my_balls)
+        for i in range(0,len(my_balls)):
+            collide_walls(my_balls[i])
+            for j in range(i+1,len(my_balls)):
+                test_collision(my_balls[i], my_balls[j])
 
         # 3. Simulate the world
         for ball in my_balls:
             ball.simulate()
 
-        # 4. Draw frame
-        # Draw Background
+        # 4. Draw
         my_win.fill(pygame.color.Color("black"))
 
-        # Draw ball
         for ball in my_balls:
-            pygame.draw.circle(my_win, ball.get_color(), (int(ball.get_x()),int(ball.get_y())), ball.get_radius())
+            pos = ball.get_position()
+            vel = ball.get_velocity().scale(50)
+            pygame.draw.circle(my_win, ball.get_color(), (int(ball.get_position().get_x()), int(ball.get_position().get_y())), ball.get_radius())
+            if draw_velocity:
+                vec = pos.add(vel)
+                pygame.draw.line(my_win, pygame.color.Color("white"), (pos.get_x(), pos.get_y()), (vec.get_x(),vec.get_y()), 3)
 
         # Swap display
         pygame.display.update()
@@ -142,6 +165,61 @@ def run_game():
     # The game loop ends here.
 
     pygame.quit()
+
+
+def test_collision(ball1, ball2):
+    # see if these two balls have collided and if so calc result
+
+    normal = ball2.get_position().sub(ball1.get_position())  # towards ball2
+
+    if normal.mag() < (ball1.get_radius() + ball2.get_radius()) and\
+            (normal.dot(ball1.get_next_velocity()) > 0 or normal.dot(ball2.get_next_velocity()) < 0):
+
+        m1 = ball1.get_mass()
+        m2 = ball2.get_mass()
+        v1 = ball1.get_next_velocity()
+        v2 = ball2.get_next_velocity()
+
+        unit_norm = normal.unit()
+        unit_tang = unit_norm.perp()
+
+        v1norm_mag = v1.dot(unit_norm)
+        v1tang_mag = v1.dot(unit_tang)
+        v2norm_mag = v2.dot(unit_norm)
+        v2tang_mag = v2.dot(unit_tang)
+
+        v1norm_mag_f = (v1norm_mag * (m1 - m2) + 2 * m2 * v2norm_mag) / (m1 + m2)
+        v1tang_mag_f = v1tang_mag
+        v2norm_mag_f = (v2norm_mag * (m2 - m1) + 2 * m1 * v1norm_mag) / (m1 + m2)
+        v2tang_mag_f = v2tang_mag
+
+        v1norm = unit_norm.scale(v1norm_mag_f)
+        v1tang = unit_tang.scale(v1tang_mag_f)
+        v2norm = unit_norm.scale(v2norm_mag_f)
+        v2tang = unit_tang.scale(v2tang_mag_f)
+
+        ball1.collide(v1norm.add(v1tang))
+        ball2.collide(v2norm.add(v2tang))
+
+
+def collide_walls(ball1):
+    # see if the ball has collided with any walls and if so calc result
+    if ball1.get_position().get_x() <= 0 + ball1.get_radius():
+        collision_vec = Vec_2D(-1, 0)
+        if collision_vec.dot(ball1.get_next_velocity()) > 0:
+            ball1.collide(ball1.get_next_velocity().add(Vec_2D(-2 * ball1.get_next_velocity().get_x(), 0)))
+    elif ball1.get_position().get_x() >= width - ball1.get_radius():
+        collision_vec = Vec_2D(1, 0)
+        if collision_vec.dot(ball1.get_next_velocity()) > 0:
+            ball1.collide(ball1.get_next_velocity().add(Vec_2D(-2 * ball1.get_next_velocity().get_x(), 0)))
+    if ball1.get_position().get_y() <= 0 + ball1.get_radius():
+        collision_vec = Vec_2D(0, -1)
+        if collision_vec.dot(ball1.get_next_velocity()) > 0:
+            ball1.collide(ball1.get_next_velocity().add(Vec_2D(0, -2 * ball1.get_next_velocity().get_y())))
+    elif ball1.get_position().get_y() >= height - ball1.get_radius():
+        collision_vec = Vec_2D(0, 1)
+        if collision_vec.dot(ball1.get_next_velocity()) > 0:
+            ball1.collide(ball1.get_next_velocity().add(Vec_2D(0, -2 * ball1.get_next_velocity().get_y())))
 
 # Start game
 run_game()
--- vectors.py
+++ vectors.py
@@ -1,3 +1,4 @@
+import math
 
 
 class Vec_2D:
@@ -11,7 +12,7 @@ class Vec_2D:
     def sub(self, other):
         return Vec_2D(self.get_x() - other.get_x(), self.get_y() - other.get_y())
 
-    def mult(self, scalar):
+    def scale(self, scalar):
         return Vec_2D(self.get_x()*scalar, self.get_y()*scalar)
 
     def dot(self, other):
@@ -24,10 +25,35 @@ class Vec_2D:
         return self.vec[1]
 
     def mag(self):
-        return (self.get_x()**2+self.get_y()**2)**(.5)
+        return (self.get_x()**2+self.get_y()**2)**.5
+
+    def angle(self):
+        #Not working!
+        print "calculating angle"
+        print "x = "+str(self.get_x())
+        print "y = "+str(self.get_y())
+
+        angle = 0
+        if self.get_x() == 0:
+            if self.get_y() > 0:
+                angle = math.pi/2
+            else:
+                angle = -1*math.pi/2
+        elif self.get_x() < 0:
+            print "need to revers"
+            print "atan = "+str(math.atan(self.get_y()/self.get_x()))
+            angle = math.atan(self.get_y()/self.get_x())+(math.pi)
+        else:
+            angle = math.atan(self.get_y()/self.get_x())
+
+        print "returning "+str(angle)
+        return angle
 
     def unit(self):
-        return self.mult(1.0/self.mag())
+        return self.scale(1.0/self.mag())
+
+    def perp(self):
+        return Vec_2D(-1*self.get_y(),self.get_x())
 
     def __str__(self):
         return "<"+str(self.get_x())+","+str(self.get_y())+">"
