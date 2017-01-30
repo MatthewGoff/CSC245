305c4d0aeaf38fec8c3928f600d51c6c0be92b43
nikolardo <nikolardoflamingo@gmail.com>
2017-01-30 15:15:56 -0500

    heyo

--- AngryBirds/crateBird.py
+++ AngryBirds/crateBird.py
@@ -8,6 +8,8 @@ import GameEngine.util
 import pyganim
 
 class crateBird(Bird):
+    MASS = 2
+    RADIUS = 50
 
     def __init__(self, position, velocity, identifier):
         #self.image = pygame.image.load("images/Bird attacks sprite sheets/Transparent PNG/Bird B/frame-1.png")
@@ -15,18 +17,17 @@ class crateBird(Bird):
                                              ('images/Bird attacks sprite sheets/Transparent PNG/Bird A/frame-2.png', 0.1),
                                              ('images/Bird attacks sprite sheets/Transparent PNG/Bird A/frame-3.png', 0.1),
                                              ('images/Bird attacks sprite sheets/Transparent PNG/Bird A/frame-4.png', 0.1),])
-        self.radius = 50
-        self.mass = 1
+
         self.animate.smoothscale((110,100))
         self.animate.play()
 
         Bird.__init__(self,
                       position,
                       velocity,
-                      self.radius,
-                      self.mass,
+                      crateBird.RADIUS,
+                      crateBird.MASS,
                       identifier)
 
 
     def update(self, window):
-        self.animate.blit(window, (self.position.get_x(), self.position.get_y()))
+        self.animate.blit(window, (self.position.x, self.position.y))
